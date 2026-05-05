f3a9a89fa2decc2d6410185ca655f48f3287753d
Juan Vivas <juandaniel200611@gmail.com>
2026-05-04 21:35:13 -0500

    Avance main modulo A

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include "graph.hpp"
+
+using namespace std;
+
+vector<Arista> ParsearArchivo(const string& archivo);
+
+void runStructuralAnalysis(const GrafoNoPonderado& graph);
+    
+int main() {
+
+    cout << "Cargando grafo" << endl;
+
+    vector<Arista> aristas = ParsearArchivo("data/roadNet-PA.txt");
+    
+    cout << "Archivo cargado correctamente." << endl;  
+
+    GrafoNoPonderado graph(aristas);
+
+    cout << "Grafo cargado correctamente." << endl;
+
+    cout << "Ejecutando modulo A" << endl;
+
+    runStructuralAnalysis(graph);
+
+    cout << "Analisis completado." << endl;
+    
+    return 0;
+}
