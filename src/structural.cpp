00003cefb6b3bcb766ebe11cd50ac3fa68efb82d
saibarrap <sebastianandresibarraprada2@gmail.com>
2026-05-04 21:33:57 -0500

    Update structural.cpp
    Added calculation for approximate diameter and connected components.

--- src/structural.cpp
+++ src/structural.cpp
@@ -13,13 +13,20 @@ void Estadisticas(GrafoNoPonderado& g){
     double prom= (2 * numA)/numN; // porque las suma de los grados es el doble de aristas
     int mayorGrado= 0;
     int grado= 0;
-    int diametroAprox;
+    int diametroAprox= 0;
     int numCompCon;
 
     for(int i= 0; i < g.lda.size(); i++){
         if(g.lda[i].size() > grado){
             mayorGrado= i;
             grado= g.lda[i].size();
+        }
+    }
+
+    vector<int> d= BFS(g, mayorGrado);
+    for(int j= 0; j < d.size(); j++){
+        if(d[j] > diametroAprox){
+            diametroAprox= d[j];
         }
     }
 }
